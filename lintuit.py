3297d52f016f32d689c5c292e3a0513a8f293e27
Steven Schaefer <stschaef@umich.edu>
2024-12-06 16:36:06 -0500

    lintuit simple parsing

--- lintuit.py
+++ lintuit.py
@@ -4,15 +4,16 @@ from z3 import *
 ## Declarations
 ll = Solver()
 F = DeclareSort("F")
+
 entails = Function("entails", F, F, BoolSort())
 tensor = Function("tensor", F, F, F)
 lpop = Function("lollipop", F, F, F)
-par = Function("par", F, F, F)  # For the ⅋ connective
-comma = Function('comma', F, F, F)
-
+oplus = Function("oplus", F, F, F)
+amp = Function("with", F, F, F)
+star = Function("*", F, F)
 
 # Variables
-x, y, z = Consts('x y z', F)
+x, y, z, w = Consts('x y z w', F)
 delta = Const('delta', F)
 gamma = Const('gamma', F)
 delta1, delta2 = Consts('delta1 delta2', F)
@@ -27,8 +28,34 @@ one = Const('1', F)
 bot = Const('⊥', F)
 empty = Const('empty', F)
 
+ll.add(ForAll([x], entails(one, star(x))))
+ll.add(ForAll([x], entails(tensor(x, star(x)), star(x))))
+ll.add(ForAll([x], entails(tensor(star(x), x), star(x))))
+ll.add(ForAll([x, y],
+    Implies(
+        And(entails(one, y), entails(tensor(x, star(x)), y)),
+        entails(star(x), y)
+    )
+))
+ll.add(ForAll([x, y],
+    Implies(
+        And(entails(one, y), entails(tensor(star(x), x), y)),
+        entails(star(x), y)
+    )
+))
+# ll.add(ForAll([x], entails(tensor(star(x), x), star(x))))
+
 ## Identity Rules
 ll.add(ForAll([x], entails(x, x)))  # (i)
+
+# transitivity
+ll.add(ForAll([x,y,z],
+    Implies(
+        And(entails(x, y), entails(y,z)),
+        entails(x, z)
+    )))
+
+# lollipop
 ll.add(
     ForAll(
         [x, y, z], entails(tensor(x, y), z) == entails(y, lpop(x, z))
@@ -36,214 +63,177 @@ ll.add(
 )  # (c)
 ## Multiplicative Rules
 
-# Rule 1 (1L)
-ll.add(ForAll([delta, gamma], Implies(entails(delta, gamma), entails(comma(delta, one), gamma))))
-
-# Rule 2 (1R)
-ll.add(entails(empty, one))
-
-# Rule 3 (⊥L)
-ll.add(entails(bot, empty))
-
-# Rule 4 (⊥R)
-ll.add(ForAll([delta, gamma], Implies(entails(delta, gamma), entails(delta, comma(bot, gamma)))))
-
-# Rule 5 (⊗L)
-ll.add(ForAll([delta, gamma, B1, B2],
-              Implies(entails(comma(comma(delta, B1), B2), gamma),
-                      entails(comma(delta, tensor(B1, B2)), gamma))))
+ll.add(ForAll([x],
+    entails(tensor(one, x), x)))
+ll.add(ForAll([x],
+    entails(tensor(x, one), x)))
+ll.add(ForAll([x],
+    entails(x, tensor(one, x))))
+ll.add(ForAll([x],
+    entails(x, tensor(x, one))))
 
 # Rule 6 (⊗R)
-ll.add(ForAll([delta1, delta2, gamma1, gamma2, B1, B2],
-              Implies(And(entails(delta1, comma(B1, gamma1)), entails(delta2, comma(B2, gamma2))),
-                      entails(comma(delta1, delta2), comma(tensor(B1, B2), comma(gamma1, gamma2))))))
-
-# Rule 7 (⅋L)
-ll.add(ForAll([delta1, delta2, gamma1, gamma2, B1, B2],
-              Implies(And(entails(comma(delta1, B1), gamma1), entails(comma(delta2, B2), gamma2)),
-                      entails(comma(comma(delta1, delta2), par(B1, B2)), comma(gamma1, gamma2)))))
+ll.add(ForAll([delta1, delta2, B1, B2],
+    Implies(And(entails(delta1, B1), entails(delta2, B2)),
+                entails(tensor(delta1, delta2), tensor(B1, B2)))))
 
 # Rule 8 (⅋R)
-ll.add(ForAll([delta, gamma, B1, B2],
-              Implies(entails(delta, comma(B1, comma(B2, gamma))),
-                      entails(delta, comma(par(B1, B2), gamma)))))
+# ll.add(ForAll([delta, gamma, B1, B2],
+#               Implies(entails(delta, comma(B1, comma(B2, gamma))),
+#                       entails(delta, comma(par(B1, B2), gamma)))))
 
 ### Additive Rules ###
-oplus = Function("oplus", F, F, F)
-amp = Function("with", F, F, F)
+
+ll.add(ForAll([x],
+    entails(oplus(zero, x), x)))
+ll.add(ForAll([x],
+    entails(oplus(x, zero), x)))
+ll.add(ForAll([x],
+    entails(x, oplus(zero, x))))
+ll.add(ForAll([x],
+    entails(x, oplus(x, zero))))
+
+ll.add(ForAll([x],
+    entails(amp(Top, x), x)))
+ll.add(ForAll([x],
+    entails(amp(x, Top), x)))
+ll.add(ForAll([x],
+    entails(x, amp(Top, x))))
+ll.add(ForAll([x],
+    entails(x, amp(x, Top))))
 
 # 0L
-ll.add(ForAll([Delta, Gamma], entails(comma(Delta, zero), Gamma)))
+ll.add(ForAll([x], entails(zero, x)))
 
 # TR
-ll.add(ForAll([Delta, Gamma], entails(Delta, comma(Top, Gamma))))
-
-# &L(i = 1,2)
-ll.add(
-    ForAll(
-        [Delta, B1, B2, Gamma],
-        Implies(
-            entails(comma(Delta, B2), Gamma),
-            entails(comma(Delta, amp(B1, B2)), Gamma),
-        ),
-    )
-)
-
-ll.add(
-    ForAll(
-        [Delta, B1, B2, Gamma],
-        Implies(
-            entails(comma(Delta, B1), Gamma),
-            entails(comma(Delta, amp(B1, B2)), Gamma),
-        ),
-    )
-)
+ll.add(ForAll([x], entails(x, Top))) 
 
 # &R
 ll.add(
     ForAll(
-        [Delta, B1, B2, Gamma],
+        [Delta, B1, B2],
         Implies(
             And(
-                entails(Delta, comma(B1, Gamma)),
-                entails(Delta, comma(B2, Gamma)),
-            ),
-            entails(Delta, comma(amp(B1, B2), Gamma)),
-        ),
+                entails(Delta, B1),
+                entails(Delta, B2)),
+            entails(Delta, amp(B1, B2))
+        )
     )
 )
-
-# (+)L
 ll.add(
     ForAll(
-        [Delta, B1, B2, Gamma],
+        [Delta, B1, B2],
         Implies(
-            And(
-                entails(comma(Delta, B1), Gamma),
-                entails(comma(Delta, B2), Gamma),
-            ),
-            entails(comma(Delta, oplus(B1, B2)), Gamma),
-        ),
+            entails(Delta, amp(B1, B2)),
+            entails(Delta, B1)
+        )
+    )
+)
+ll.add(
+    ForAll(
+        [Delta, B1, B2],
+        Implies(
+            entails(Delta, amp(B1, B2)),
+            entails(Delta, B2)
+        )
     )
 )
 
 # (+)R(i = 1,2)
 ll.add(
     ForAll(
-        [Delta, B1, B2, Gamma],
+        [Delta, B1, B2],
         Implies(
-            entails(Delta, comma(B2, Gamma)),
-            entails(Delta, comma(oplus(B1, B2), Gamma)),
-        ),
-    )
+            entails(Delta, B2),
+            entails(Delta, oplus(B1, B2))
+        )
+    ),
 )
 
 ll.add(
     ForAll(
-        [Delta, B1, B2, Gamma],
+        [Delta, B1, B2],
         Implies(
-            entails(Delta, comma(B1, Gamma)),
-            entails(Delta, comma(oplus(B1, B2), Gamma)),
-        ),
+            entails(Delta, B1),
+            entails(Delta, oplus(B1, B2))
+        )
+    ),
+)
+
+ll.add(
+    ForAll(
+        [x,y,z,w],
+        Implies(
+            And(
+                entails(w, oplus(x,y)),
+                entails(x,z),
+                entails(y,z)
+            ),
+            entails(w,z)
+        )
     )
 )
 
 ### Exponential Rules ###
-em = Function("em", F, F)
-qm = Function("qm", F, F)
+# qm = Function("qm", F, F)
+
+# ll.add(ForAll([x], entails(x, em(x))))
+# ll.add(ForAll([x], entails(em(x), tensor(em(x), em(x)))))
+# ll.add(ForAll([x], entails(tensor(em(x), em(x)), em(x))))
 
 # !W
-ll.add(
-    ForAll(
-        [Delta, Gamma, B],
-        Implies(
-            entails(Delta, Gamma),
-            entails(comma(Delta, em(B)), Gamma),
-        ),
-    )
-)
+# ll.add(
+#     ForAll(
+#         [Delta, Gamma, B],
+#         Implies(
+#             entails(Delta, Gamma),
+#             entails(comma(Delta, em(B)), Gamma),
+#         ),
+#     )
+# )
 
-# !C
-ll.add(
-    ForAll(
-        [Delta, Gamma, B],
-        Implies(
-            entails(comma(Delta, comma(em(B), em(B))), Gamma),
-            entails(comma(Delta, em(B)), Gamma),
-        ),
-    )
-)
+# # !C
+# ll.add(
+#     ForAll(
+#         [Delta, Gamma, B],
+#         Implies(
+#             entails(comma(Delta, comma(em(B), em(B))), Gamma),
+#             entails(comma(Delta, em(B)), Gamma),
+#         ),
+#     )
+# )
 
-# !D
-ll.add(
-    ForAll(
-        [Delta, Gamma, B],
-        Implies(
-            entails(comma(Delta, B), Gamma),
-            entails(comma(Delta, em(B)), Gamma),
-        ),
-    )
-)
+# # !D
+# ll.add(
+#     ForAll(
+#         [Delta, Gamma, B],
+#         Implies(
+#             entails(comma(Delta, B), Gamma),
+#             entails(comma(Delta, em(B)), Gamma),
+#         ),
+#     )
+# )
 
-# !R
-ll.add(
-    ForAll(
-        [Delta, Gamma, B],
-        Implies(
-            entails(em(Delta), comma(B, qm(Gamma))),
-            entails(em(Delta), comma(em(B), qm(Gamma))),
-        ),
-    )
-)
+# # !R
+# ll.add(
+#     ForAll(
+#         [Delta, Gamma, B],
+#         Implies(
+#             entails(em(Delta), comma(B, qm(Gamma))),
+#             entails(em(Delta), comma(em(B), qm(Gamma))),
+#         ),
+#     )
+# )
 
-# ?W
-ll.add(
-    ForAll(
-        [Delta, Gamma, B],
-        Implies(
-            entails(Delta, Gamma),
-            entails(Delta, comma(qm(B), Gamma)),
-        ),
-    )
-)
+# ll.add(Not(entails(tensor(x,one), x))) # unsat
+# ll.add(Not(entails(one, lpop(x, x)))) # unsat
+# ll.add(Not(entails(x, star(x)))) # unsat
+def mkStr(n):
+    if n == 0:
+        return one
+    # return tensor(tensor(x,y), mkStr(n - 1))
+    return tensor(mkStr(n - 1) ,tensor(x,y))
 
-# ?C
-ll.add(
-    ForAll(
-        [Delta, Gamma, B],
-        Implies(
-            entails(Delta, comma(comma(qm(B), qm(B)), Gamma)),
-            entails(Delta, comma(qm(B), Gamma)),
-        ),
-    )
-)
-
-# ?D
-ll.add(
-    ForAll(
-        [Delta, Gamma, B],
-        Implies(
-            entails(Delta, comma(B, Gamma)),
-            entails(Delta, comma(qm(B), Gamma)),
-        ),
-    )
-)
-
-# ?L
-ll.add(
-    ForAll(
-        [Delta, Gamma, B],
-        Implies(
-            entails(comma(em(Delta), B), qm(Gamma)),
-            entails(comma(em(Delta), qm(B)), qm(Gamma)),
-        ),
-    )
-)
-
-test_solver = Solver()
-assertions = ll.assertions()
-test_solver.add(assertions)
-print(ll.check())
-ll.add(Not(ForAll([x], entails(tensor(x,empty), x))))
-
+ll.add(Not(entails(mkStr(5), star(tensor(x, y))))) # unsat up to n = 25
+# ll.add(Not(entails(star(tensor(x,x)), star(x))))
 print(ll.check())
